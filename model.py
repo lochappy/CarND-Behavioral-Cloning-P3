6439ae35f588fe11cf986a7f3ed31b1f1d9ecda4
lochappy@server <ttanloc@gmail.com>
2017-03-24 20:16:13 +0800

    The first working model was done

--- model.py
+++ model.py
@@ -7,8 +7,78 @@ Created on Wed Mar 22 15:18:00 2017
 
 import csv, cv2, h5py
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Flatten, Lambda, Activation, pooling, PReLU, Cropping2D, Dropout
 
-dataFolder = './sample_data'
+def LeNet():
+    ''' Return lenet architecture '''
+    model = Sequential()
+    model.add(Lambda(lambda x: (x/255.0 - 0.5),input_shape=(160,320,3)))
+    
+    model.add(Cropping2D(cropping=((50,25),(0,0))))
+    
+    model.add(Conv2D(24,5,5))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Conv2D(48,3,3))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Conv2D(96,3,3))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Flatten())
+    model.add(Dense(256))
+    #model.add(PReLU())
+    model.add(Dense(128))
+    model.add(Dense(64))
+    model.add(Dense(1))
+    
+    return model
+    
+def NvidiaDriving():
+    ''' Return NvidiaDriving architecture '''
+    model = Sequential()
+    model.add(Lambda(lambda x: (x/255.0 - 0.5),input_shape=(160,320,3)))
+    
+    model.add(Cropping2D(cropping=((50,25),(0,0))))
+    
+    model.add(Conv2D(24,5,5))#, subsample=(2,2)))
+    #model.add(Activation('relu'))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Conv2D(36,3,3))#, subsample=(2,2)))
+    #model.add(Activation('relu'))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Conv2D(48,3,3))#, subsample=(2,2)))
+    #model.add(Activation('relu'))
+    model.add(PReLU())
+    model.add(pooling.MaxPool2D())
+    
+    model.add(Conv2D(64,3,3))
+    #model.add(Activation('relu'))
+    model.add(PReLU())
+    
+    model.add(Conv2D(64,3,3))
+    #model.add(Activation('relu'))
+    model.add(PReLU())
+    
+    model.add(Flatten())
+    model.add(Dense(100))
+    model.add(Dense(50))
+    model.add(Dropout(0.5))
+    model.add(Dense(10))
+    model.add(Dense(1))
+    
+    return model
+    
+
+dataFolder = './my_data'
 with open(dataFolder + '/driving_log.csv') as f:
     reader = csv.reader(f)
     next(reader) #this skip the header
@@ -17,7 +87,7 @@ with open(dataFolder + '/driving_log.csv') as f:
 from random import shuffle
 shuffle(dataAnn)
 
-center_images = np.array([cv2.imread(dataFolder+'/'+line[0]) for line in dataAnn],dtype=np.float32)
+center_images = np.array([cv2.imread(line[0]) for line in dataAnn],dtype=np.float32)
 steering_angles = np.array([float(line[3]) for line in dataAnn],dtype=np.float32)
 
 flipped_center_images = np.array([np.fliplr(img)for img in center_images],dtype=np.float32)
@@ -32,27 +102,14 @@ flipped_steering_angles = -steering_angles
 X_train = np.append(center_images,flipped_center_images,axis=0)
 Y_train = np.append(steering_angles,flipped_steering_angles,axis=0)
 
-from keras.models import Sequential
-from keras.layers import Conv2D, Dense, Flatten, Lambda, Activation, pooling, PReLU
 
-model = Sequential()
-model.add(Lambda(lambda x: (x/255.0 - 0.5),input_shape=(160,320,3)))
 
-model.add(Conv2D(6,5,5))
-model.add(PReLU())
-model.add(pooling.MaxPool2D())
+model = NvidiaDriving()
 
-model.add(Conv2D(6,5,5))
-model.add(PReLU())
-model.add(pooling.MaxPool2D())
+from keras import optimizers
+adam = optimizers.Adam(lr=0.0001)
 
-model.add(Flatten())
-model.add(Dense(120))
-#model.add(PReLU())
-model.add(Dense(84))
-model.add(Dense(1))
-
-model.compile(loss='mse',optimizer='adam')
+model.compile(loss='mse',optimizer=adam)
 
 '''
 saves the model weights after each epoch if the validation loss decreased
@@ -60,7 +117,7 @@ saves the model weights after each epoch if the validation loss decreased
 from keras.callbacks import ModelCheckpoint
 checkpointer = ModelCheckpoint(filepath="model.h5", verbose=1, save_best_only=True)
 
-model.fit(x=X_train,y=Y_train,validation_split=0.2,shuffle=True,nb_epoch=5,callbacks=[checkpointer])
+model.fit(x=X_train,y=Y_train,validation_split=0.1,shuffle=True,nb_epoch=15,callbacks=[checkpointer],batch_size=64)
 
 #model.save('behavior_cloning_model.h5')
 
--- video.py
+++ video.py
@@ -1,5 +1,7 @@
 from moviepy.editor import ImageSequenceClip
-import argparse
+import argparse, os
+import numpy as np
+import matplotlib.image as cimg
 
 
 def main():
@@ -19,7 +21,11 @@ def main():
 
     video_file = args.image_folder + '.mp4'
     print("Creating video {}, FPS={}".format(video_file, args.fps))
-    clip = ImageSequenceClip(args.image_folder, fps=args.fps)
+    imgPaths = sorted([os.path.join(root, name) for root, dirs, files in os.walk(args.image_folder) 
+                                for name in files 
+                                if name.split('.')[-1] in ['jpg', 'png', 'jpeg']])
+    imgs = [cimg.imread(imgPath) for imgPath in imgPaths]
+    clip = ImageSequenceClip(imgs, fps=args.fps)
     clip.write_videofile(video_file)
 
 
